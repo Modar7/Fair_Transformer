bbfca874fdfbfbc80c56c2391043d7fde8ab00bc
Modar Sulaiman <58742371+Modar7@users.noreply.github.com>
2022-05-28 17:57:51 +0300

    Add files via upload

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+from src.models.tab_mlp import TabMlp
+from src.models.Tabular_Transformer import TabModel
+from src.models.tabnet.tab_net import TabNet
+from src.models.transformers.saint import SAINT
+from src.models.transformers.tab_perceiver import TabPerceiver
+from src.models.transformers.ft_transformer import FTTransformer
+from src.models.transformers.tab_transformer import TabTransformer
