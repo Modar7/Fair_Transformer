f35061f0983ac96e169df676ca7f091a4d2a2628
Modar Sulaiman <58742371+Modar7@users.noreply.github.com>
2022-05-28 19:23:18 +0300

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,2 @@
-#from src.utils.text_utils import *
-#from src.utils.image_utils import *
-from src.utils.deeptabular_utils import *
+from src.utils.tabular_utils import *
 from src.utils.fastai_transforms import *
